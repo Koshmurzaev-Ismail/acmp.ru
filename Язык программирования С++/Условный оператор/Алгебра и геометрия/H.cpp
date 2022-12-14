1f06050c458fa6add5ea7eaf7ffd25848011d20d
Ismail <ismailkoshmurzaev@gmail.com>
2022-12-14 21:03:38 +0600

    OK

--- Язык программирования С++/Условный оператор/Алгебра и геометрия/H.cpp
+++ Язык программирования С++/Условный оператор/Алгебра и геометрия/H.cpp
@@ -1,50 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-    double a,b,c;
-    double x1,x2,d;
-    cin >> a >> b >> c;
-    if(a == 0) {
-      if (b == 0) 
-        if (c == 0) cout << -1;
-        else cout << 0;
-      else {
-            cout << 1 << endl;
-            if(c != 0)
-                x1 = -c / b;
-            else
-                x1 = 0;
-            cout << fixed << setprecision(11) << x1;
-        }
-      } 
-    else if(a != 0) {
-        d = b * b - 4.0 * a * c;
-        if(d > 0){
-            cout << 2 << endl;
-            x1 = (-b - sqrt(d))/(2 * a);
-            x2 = (-b + sqrt(d))/(2 * a);
-                if(x1 != 0)
-                    cout << fixed << setprecision(11) << x1;
-                else
-                    cout <<fixed << setprecision(11) << 0;
-            cout << endl;
-            if(x2 != 0)
-                cout <<fixed <<  setprecision(11) << x2;
-                else
-                    cout <<fixed << setprecision(11) <<  0;
-        }
-        else
-            if(d == 0){
-                cout << 1 << endl;
-                x1 = -b / (2 * a);
-                if(x1)
-                    cout <<fixed << setprecision(11) <<  x1;
-                else
-                    cout << fixed << setprecision(11) << 0;
-            }
-            else
-                if(d < 0)
-                    cout << 0;
+  long long a, b, c;
+  cin >> a >> b >> c;
+  if (a == 0) {
+    if (b == 0) {
+      if (c == 0) cout << -1;
+      else cout << 0;
+    } else {
+      cout << 1 << endl;
+      cout << -c * 1.0 / b;
     }
+  } else {
+    long long d = b * b - 4 * a * c;
+    if (d < 0) cout << 0;
+    else if (d == 0) {
+      cout << 1 << endl;
+      cout << -b * 1.0 / (2.0 * a);
+    }
+    else {
+      double x1 = (-b - sqrt(d)) / (2.0 * a);
+      double x2 = (-b + sqrt(d)) / (2.0 * a);
+      cout << 2 << endl;
+      cout << x1 << endl;
+      cout << x2;
+    }
+  }
     return 0;
 }
